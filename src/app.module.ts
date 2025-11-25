42dc6e2c49c1ce28c4b4dadded2c084432685aef
nativealien <oskar@assarsson.nu>
2025-11-25 07:24:14 +0100

    interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { User } from './users/users.entity';
+import { Product } from './products/products.entity';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    entities: [User, Product],
+    synchronize: true,
+  }), UsersModule, AuthModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+    @IsEmail()
+    @IsOptional()
+    email: string;
+    @IsString()
+    @IsOptional()
+    @MinLength(8)
+    password: string;
+}
