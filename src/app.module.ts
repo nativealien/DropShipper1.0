4daa3085f5424df29c3da53df1b3bbd30ca45c5d
nativealien <oskar@assarsson.nu>
2025-12-03 06:54:27 +0100

    added docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './routes/users/user.entity';
+import { UsersModule } from './routes/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'devuser',
+      password: 'devpass',
+      database: 'devdb',
+      entities: [User],
+      synchronize: true, // dev only!
+      logging: true,     // optional but nice in dev
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
